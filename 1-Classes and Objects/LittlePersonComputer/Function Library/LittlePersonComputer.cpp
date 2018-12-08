abd5f5312aa968f5df71d57534246de0daf5bc45
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-07 17:55:54 -0800

    Update LittlePersonComputer.cpp

--- 1-Classes and Objects/LittlePersonComputer/Function Library/LittlePersonComputer.cpp
+++ 1-Classes and Objects/LittlePersonComputer/Function Library/LittlePersonComputer.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include "LittlePersonComputer.h"
 using namespace std;
+
+// defult class state
+LittlePersonComputer::LittlePersonComputer(){
+    for(int i = 0; i < MEMORYSIZE; i++) {
+        memory[i] = 0;
+    }
+    accumulator = 0;
+    programCounter = 0;
+}
+
+// initialized class state
+LittlePersonComputer::LittlePersonComputer(int instructions[], int numberOfInstructions){
+    accumulator = 0;
+    programCounter = 0;
+
+    for(int i = 0; i < numberOfInstructions; i++){
+        memory[i] = instructions[i];
+    }
+
+}
