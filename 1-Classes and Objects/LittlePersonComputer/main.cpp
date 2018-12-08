463bb1e0a61f12a391b0f379d2d7dbe5f8344cff
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-08 15:08:48 -0800

    Update main.cpp

--- 1-Classes and Objects/LittlePersonComputer/main.cpp
+++ 1-Classes and Objects/LittlePersonComputer/main.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    LittlePersonComputer computer1;
+
+    computer1.printState();
+
+    //cout << computer1.getProgramCounter() << endl;
+
+    //cout << computer1.isHalted() << endl;
+
+    int program[] = { 901, 307, 506, 207, 902, 0 , 10 };
+    int programLength = 7;
+    computer1.loadProgram(program, programLength);
+
+    computer1.printState();
+
+    cout << "Memory at 2 = " <<
+    computer1.getMemoryAt(2)<< endl;
+
+    computer1.step();
+    computer1.printState();
 }
