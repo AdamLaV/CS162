1d2ea61d55ef00b513e48d6a4bf839c445af73c5
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-13 16:53:15 -0800

    Create notes.cpp

---
+++ 3-Pointers/notes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // normal varibles
+    int count = 5, count2 = 10;
+
+    // pointer variable
+    int* pCount;
+    int* pCount2;
+
+    cout << pCount << "  " << pCount2 << endl;
+
+    // assiging address of normal variable to pointers
+    pCount = &count;
+    pCount2 = &count2;
+
+    cout << count << " is at location " << pCount << " \n"
+         << count2 << " is at location " << pCount2 << endl;
+}
