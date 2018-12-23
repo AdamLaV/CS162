2b308bc292a4c3688ec46adbd9f7a8f75b913bd2
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-23 15:27:26 -0800

    Update notes.cpp

--- 3-Pointers/notes.cpp
+++ 3-Pointers/notes.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
-{
+{ 
     // normal varibles
     int count = 5, count2 = 10;
+    char letter = 'A';
 
     // pointer variable
     int* pCount;
     int* pCount2;
+    char* pLetter;
+
+    //cout << pCount << "  " << pCount2 << endl;
 
     // assiging address of normal variable to pointers
     pCount = &count;
     pCount2 = &count2;
+    pLetter = &letter;
 
-    cout << count << " is at location " << pCount << " \n"
-         << count2 << " is at location " << pCount2 << endl;
+    cout << count << " is at location " << pCount << "\n"
+         << count2 << " is at location " << pCount2 << "\n"
+         << letter << " is at location " << pLetter << endl;
 }
-
-
-// typedef and pointers
-typedef int* intPointers;
 
