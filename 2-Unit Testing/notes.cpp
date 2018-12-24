370a4a1596fc1bee50f489d8db8d4de23a6580fd
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-23 16:45:33 -0800

    Update notes.cpp

--- 2-Unit Testing/notes.cpp
+++ 2-Unit Testing/notes.cpp
@@ -3,3 +3,12 @@
 TEST_CASE( "UniqueMachineName" ) {
     //Do any setup we need to before we can test what we want
     REQUIRE( something is true );
+    
+// Example 
+    
+    TEST_CASE( "Circle/DefaultConstructor" ) {
+    Circle c1;
+    REQUIRE( c1.getX() == 0 );
+    REQUIRE( c1.getY() == 0 );
+    REQUIRE( c1.getRadius() == 2 );
+}
