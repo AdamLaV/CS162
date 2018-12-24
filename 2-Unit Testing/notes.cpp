847da7dd5c35acf4aec61a873492e1ec6ce14825
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-23 16:44:30 -0800

    Update notes.cpp

--- 2-Unit Testing/notes.cpp
+++ 2-Unit Testing/notes.cpp
@@ -1,2 +1,5 @@
-//
-//
+// After Connecting each piece to the pogram, each test should look like this:
+
+TEST_CASE( "UniqueMachineName" ) {
+    //Do any setup we need to before we can test what we want
+    REQUIRE( something is true );
