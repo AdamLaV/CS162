328b123298d6f2d991584fbc6b7516701190510c
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-24 17:56:07 -0800

    Update Rectangle.cpp

--- 2-Unit Testing/Rectangle/Function Library/Rectangle.cpp
+++ 2-Unit Testing/Rectangle/Function Library/Rectangle.cpp
@@ -2,3 +2,58 @@
   * @brief Assignment 2
   * @author Daidemang Francis
 */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#include "Point.h"
+#include "Rectangle.h"
+
+
+// rectangle constructor
+Rectangle::Rectangle(Point& p1, double heightValue, double widthValue) {
+    upperLeftVertex = p1;
+    height = heightValue;
+    width = widthValue;
+}
+
+// rectangle constructor
+Rectangle::Rectangle(Point& p1, Point& p2) {
+    upperLeftVertex = p1;
+    height = abs(p1.getY() - p2.getY());
+    width = abs(p1.getX() - p2.getX());
+}
+
+// getUpperLeftVertex method
+Point Rectangle::getUpperLeftVertex()const {
+    return upperLeftVertex;
+}
+
+// getWidth method
+double Rectangle::getWidth()const {
+    return width;
+}
+
+// getHeight method
+double Rectangle::getHeight()const {
+    return height;
+}
+
+// getArea method
+double Rectangle::getArea() {
+    return width * height;
+}
+
+// getPerimeter method
+double Rectangle::getPerimeter() {
+    return (width + height) * 2;
+}
+
+// getCenter method
+Point Rectangle::getCenter() {
+    Point newPoint;
+    newPoint.getX();
+    newPoint.getY();
+
+    return newPoint;
+}
