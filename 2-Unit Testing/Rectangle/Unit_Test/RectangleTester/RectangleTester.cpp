86ed3d53c53082d521432e13ac7bcb548e4e860b
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-25 16:16:50 -0800

    Update RectangleTester.cpp

--- 2-Unit Testing/Rectangle/Unit_Test/RectangleTester/RectangleTester.cpp
+++ 2-Unit Testing/Rectangle/Unit_Test/RectangleTester/RectangleTester.cpp
@@ -5,11 +5,22 @@
 #include "Rectangle.h"
 #include "Point.h"
 
-TEST_CASE( "SAMPLE/test post-increment" ) {
+TEST_CASE( "Rectangle/Constructor1", "3 argument constructor" ) {
 
-    int x = 1;
-    x++;
+    Point p1(1, 5);
+    Rectangle r1(p1, 2, 4);
+    Point temp = r1.getUpperLeftVertex();
+    REQUIRE( temp.isSameAs(p1) == true);
 
-    //Make sure that x really equals 2
-    REQUIRE( x == 2 );
+}
+
+TEST_CASE( "Rectangle/Constructor2","2 argument constructor"  ) {
+
+    Point p1(5, 5);
+    Point p2(5, 5);
+    Rectangle r2(p1, p2);
+    Point temp2 = r2.getUpperLeftVertex();
+    REQUIRE( temp2.isSameAs(p1) == true);
+    REQUIRE( abs(p1.getY() - p2.getY()) == r2.getWidth());
+    REQUIRE( abs(p1.getX() - p2.getX()) == r2.getHeight());
 }
