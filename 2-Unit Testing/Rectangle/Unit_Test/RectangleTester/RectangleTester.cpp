53cff4ed0bddaf73446ad20563625fe6e55b0a45
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-25 16:55:21 -0800

    Update RectangleTester.cpp

--- 2-Unit Testing/Rectangle/Unit_Test/RectangleTester/RectangleTester.cpp
+++ 2-Unit Testing/Rectangle/Unit_Test/RectangleTester/RectangleTester.cpp
@@ -1,3 +1,4 @@
+
 //Bring in unit testing code
 #include "catch.hpp"
 
@@ -5,9 +6,15 @@
 #include "Rectangle.h"
 #include "Point.h"
 
+using namespace std;
+
+
+Point p1(1, 2);
+Point p2(6, -2);
+Rectangle r2(p1, p2);
+
 TEST_CASE( "Rectangle/Constructor1", "3 argument constructor" ) {
 
-    Point p1(1, 5);
     Rectangle r1(p1, 2, 4);
     Point temp = r1.getUpperLeftVertex();
     REQUIRE( temp.isSameAs(p1) == true);
@@ -16,11 +23,22 @@ TEST_CASE( "Rectangle/Constructor1", "3 argument constructor" ) {
 
 TEST_CASE( "Rectangle/Constructor2","2 argument constructor"  ) {
 
-    Point p1(5, 5);
-    Point p2(5, 5);
-    Rectangle r2(p1, p2);
     Point temp2 = r2.getUpperLeftVertex();
     REQUIRE( temp2.isSameAs(p1) == true);
-    REQUIRE( abs(p1.getY() - p2.getY()) == r2.getWidth());
-    REQUIRE( abs(p1.getX() - p2.getX()) == r2.getHeight());
+    REQUIRE( r2.getWidth() == Approx(5));
+    REQUIRE( r2.getHeight() == Approx(4));
 }
+
+TEST_CASE( "Rectangle/getUpperLeftVertex method" ) {
+    Point temp2 = r2.getUpperLeftVertex();
+    REQUIRE( temp2.isSameAs(r2.getUpperLeftVertex()));
+}
+
+TEST_CASE ( "Rectangle/getWidth method") {
+    REQUIRE( r2.getWidth() == Approx(5));
+}
+
+TEST_CASE ( "Rectangle/getHeigt method") {
+    REQUIRE( r2.getHeight() == Approx(4));
+}
+
