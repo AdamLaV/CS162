d6b29152e399a460f3be3e18aa9694e0118b420a
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-26 14:30:20 -0800

    Update notes.cpp

--- 3-Pointers/notes.cpp
+++ 3-Pointers/notes.cpp
@@ -26,5 +26,105 @@ int main()
          << letter << " is at location " << pLetter << endl;
 }
 
-// Aggregation
+// Aggregation Example 
+
+//-------------------------------------------------------------------------------------------------
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+	
+#include <string>
+#include <iostream>
+ 
+class Teacher
+{
+private:
+    std::string m_name;
+ 
+public:
+    Teacher(std::string name)
+        : m_name(name)
+    {
+    }
+ 
+    std::string getName() { return m_name; }
+};
+ 
+class Department
+{
+private:
+    Teacher *m_teacher; // This dept holds only one teacher for simplicity, but it could hold many teachers
+ 
+public:
+    Department(Teacher *teacher = nullptr)
+        : m_teacher(teacher)
+    {
+    }
+};
+ 
+int main()
+{
+    // Create a teacher outside the scope of the Department
+    Teacher *teacher = new Teacher("Bob"); // create a teacher
+    {
+        // Create a department and use the constructor parameter to pass
+        // the teacher to it.
+        Department dept(teacher);
+ 
+    } // dept goes out of scope here and is destroyed
+ 
+    // Teacher still exists here because dept did not delete m_teacher
+ 
+    std::cout << teacher->getName() << " still exists!";
+ 
+    delete teacher;
+ 
+    return 0;
+}
+
+
 
