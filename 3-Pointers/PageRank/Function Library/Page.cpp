4b4048af190f13972734fbbb8b8454b47370aee4
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-26 17:03:43 -0800

    Update Page.cpp

--- 3-Pointers/PageRank/Function Library/Page.cpp
+++ 3-Pointers/PageRank/Function Library/Page.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 #include "Page.h"
 
+// default constructor
 Page::Page() {
     this->url = "";
     this->visits = 0;
     this->usedLinks = 0;
+
+    for(size_t i = 0; i < 3; i++) {
+        links[i] = nullptr;
+    }
 }
+
+Page::Page(string urlValue) {
+    this->url = urlValue;
+    this->visits = 0;
+    this->usedLinks = 0;
+
+    for(size_t i = 0; i < 3; i++) {
+        links[i] = nullptr;
+    }
+}
+
