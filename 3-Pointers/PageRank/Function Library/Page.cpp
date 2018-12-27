e37e3de22c7a0309fa4fdd1da89a1dfb5fe83c8f
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-27 15:52:47 -0800

    Update Page.cpp

--- 3-Pointers/PageRank/Function Library/Page.cpp
+++ 3-Pointers/PageRank/Function Library/Page.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 #include "Page.h"
 
@@ -8,7 +10,7 @@ Page::Page() {
     this->visits = 0;
     this->usedLinks = 0;
 
-    for(size_t i = 0; i < 3; i++) {
+    for(size_t i = 0; i < LINKSIZE; i++) {
         links[i] = nullptr;
     }
 }
@@ -19,8 +21,38 @@ Page::Page(string urlValue) {
     this->visits = 0;
     this->usedLinks = 0;
 
-    for(size_t i = 0; i < 3; i++) {
+    for(size_t i = 0; i < LINKSIZE; i++) {
         links[i] = nullptr;
     }
+}
+
+// getURL method
+string Page::getURL() {
+    return this->url;
+}
+
+// getNumLinks method
+int Page::getNumLinks() {
+    return this->usedLinks;
+}
+
+// addLink method
+void Page::addLink(Page *other) {
+    for(size_t i = 0; i < LINKSIZE; i++) {
+        if(links[i] == nullptr) {
+            links[i] = other;
+            usedLinks++;
+            break;
+        }
+    }
+}
+
+// getRandomLink method
+Page* Page::getRandomLink() {
+    srand(time(NULL));
+    int RandIndex = rand() % 3;
+
+    Page* randomAddress = links[RandIndex];
+    return randomAddress;
 }
 
