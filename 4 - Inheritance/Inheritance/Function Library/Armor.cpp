6d1ab00bce8ea9ea283a704690012e2f248d3745
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-31 15:37:01 -0800

    Create Armor.cpp

---
+++ 4 - Inheritance/Inheritance/Function Library/Armor.cpp
@@ -0,0 +1,19 @@
+#include "Armor.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Armor default constructor
+Armor::Armor(const string& name, int protectionValue, int cost) :Item(name, cost) {
+    this->protection = protectionValue;
+}
+
+// toString method
+string Armor::toString() {
+    return this->getName() + " (value: " + to_string(this->getValue()) + ") Protection: " + to_string(this->getProtection());
+}
+
+// getProtection method
+int Armor::getProtection() {
+    return this->protection;
+}
