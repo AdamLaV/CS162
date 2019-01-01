0e7a3a45ff73a169f1627537e03a8fc833e46319
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-01-01 14:25:53 -0800

    Create Weapon.cpp

---
+++ 4 - Inheritance/Inheritance/Function Library/Weapon.cpp
@@ -0,0 +1,37 @@
+#include "Weapon.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+// default constructor
+Weapon::Weapon(string name, int damageValue, int cost) : Item(name, cost) {
+    this->damage = damageValue;
+}
+
+int Weapon::getDamage() {
+    return this->damage;
+}
+
+// toString method
+string Weapon::toString() {
+    return this->getName() + " (value: " + to_string(this->getValue()) + ") Damage: " + to_string(this->getDamage());
+}
+
+int Weapon::getRange() {
+    return 0;
+}
+
+int Weapon::use(int rangeToTarget) {
+    int range = getRange();
+    int damageDone = 0;
+    if(rangeToTarget < range) {
+        damageDone = getDamage();
+    }
+    else if (rangeToTarget >= range) {
+        damageDone = getDamage();
+        damageDone -= rangeToTarget;
+        if(damageDone < 0) damageDone = 0;
+    }
+
+    return damageDone;
+}
