025b77483349ef234d85e6f67229becf03d6e68b
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-01-04 18:21:27 -0800

    Update RangedWeapon.cpp

--- 4 - Inheritance/Inheritance/Function Library/RangedWeapon.cpp
+++ 4 - Inheritance/Inheritance/Function Library/RangedWeapon.cpp
@@ -9,3 +9,42 @@ RangedWeapon::RangedWeapon(string name, int damageValue, int rangeValue, int amm
     this->range = rangeValue;
     this->ammo = ammoValue;
 }
+
+// toString method
+string RangedWeapon::toString() {
+    return this->getName() + " (value: " + to_string(this->getValue()) +
+            ") Damage: " + to_string(this->getDamage()) + " Ammo: " +
+            to_string(this->getAmmo()) + " Range: " + to_string(this->getRange());
+}
+
+// getRange method
+int RangedWeapon::getRange() {
+    return this->range;
+}
+
+// getAmmo method
+int RangedWeapon::getAmmo() {
+    return this->ammo;
+}
+
+// use method
+int RangedWeapon::use(int rangeToTarget) {
+    // get ammo status and compute findings
+    int damageDone = getDamage();
+    int myDamage = 0;
+    if(rangeToTarget <= getRange() && ammo > 0) {
+        this->ammo--;
+        myDamage = damageDone;
+    }
+
+    else if (rangeToTarget > getRange() && ammo > 0) {
+        this->ammo--;
+        myDamage = 0;
+    }
+
+    else {
+        myDamage = 0;
+    }
+
+    return myDamage;
+}
