4ef0e6c2cbaf80e41ee10359f573b188de2794ac
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-01-04 16:58:37 -0800

    Update RangedWeapon.cpp

--- 4 - Inheritance/Inheritance/Function Library/RangedWeapon.cpp
+++ 4 - Inheritance/Inheritance/Function Library/RangedWeapon.cpp
@@ -3,3 +3,9 @@
 using namespace std;
 #include "RangedWeapon.h"
 
+// default constructor
+RangedWeapon::RangedWeapon(string name, int damageValue, int rangeValue, int ammoValue, int cost)
+    : Weapon(name, damageValue, cost) {
+    this->range = rangeValue;
+    this->ammo = ammoValue;
+}
