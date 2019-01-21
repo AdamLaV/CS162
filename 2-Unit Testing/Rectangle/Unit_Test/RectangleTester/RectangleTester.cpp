8d47f6b9fb44f17e3ce46b63eb645aac8ff5ca65
Daidemang Francis <dfranc15@my.chemeketa.edu>
2019-01-21 14:07:56 -0800

    Update RectangleTester.cpp

--- 2-Unit Testing/Rectangle/Unit_Test/RectangleTester/RectangleTester.cpp
+++ 2-Unit Testing/Rectangle/Unit_Test/RectangleTester/RectangleTester.cpp
@@ -16,49 +16,53 @@ TEST_CASE( "Rectangle/Constructor1", "3 argument constructor" ) {
 
     Rectangle r1(p1, 2, 4);
     Point temp = r1.getUpperLeftVertex();
-    REQUIRE( temp.isSameAs(p1) == true);
+    REQUIRE(temp.isSameAs(p1) == true);
 }
 
 TEST_CASE( "Rectangle/Constructor2","2 argument constructor"  ) {
 
     Point temp2 = r2.getUpperLeftVertex();
-    REQUIRE( temp2.isSameAs(p1) == true);
-    REQUIRE( r2.getWidth() == Approx(5));
-    REQUIRE( r2.getHeight() == Approx(4));
+    REQUIRE(temp2.isSameAs(p1) == true);
+    REQUIRE(r2.getWidth() == Approx(5));
+    REQUIRE(r2.getHeight() == Approx(4));
 }
 
-TEST_CASE( "Rectangle/getUpperLeftVertex method" ) {
+TEST_CASE( "Rectangle/getUpperLeftVertex" ) {
     Point temp2 = r2.getUpperLeftVertex();
-    REQUIRE( temp2.isSameAs(r2.getUpperLeftVertex()));
+    REQUIRE(temp2.isSameAs(r2.getUpperLeftVertex()));
 }
 
-TEST_CASE ( "Rectangle/getWidth method") {
-    REQUIRE( r2.getWidth() == Approx(5));
+TEST_CASE ( "Rectangle/getWidth") {
+    REQUIRE(r2.getWidth() == Approx(5));
 }
 
-TEST_CASE ( "Rectangle/getHeigt method") {
-    REQUIRE( r2.getHeight() == Approx(4));
+TEST_CASE ( "Rectangle/getHeigt") {
+    REQUIRE(r2.getHeight() == Approx(4));
 }
 
-TEST_CASE ( "Rectangle/getArea method") {
+TEST_CASE ( "Rectangle/getArea") {
     REQUIRE(r2.getWidth() * r2.getHeight() == Approx(20));
 }
 
-TEST_CASE ( "Rectangle/getPerimeter method") {
+TEST_CASE ( "Rectangle/getPerimeter") {
      REQUIRE((r2.getWidth() + r2.getHeight()) * 2 == Approx(18));
 }
 
-//TEST_CASE ( "Rectangle/getCenter") {
-//    Point p3;
-//    p3.getX();
-//    p3.getY();
+TEST_CASE ( "Rectangle/getCenter") {
+    REQUIRE((p1.getX() + r2.getWidth()) / 2 == Approx(3.0));
+    REQUIRE((p1.getY() + r2.getHeight()) / 2 == Approx(3.0));
+}
 
-//    REQUIRE(p3.;
-//}
-
-TEST_CASE ( "Rectangle/shift method") {
+TEST_CASE ( "Rectangle/shift") {
     r2.shift(2.2, 4.2);
     REQUIRE(r2.getHeight() == Approx(6.2));
     REQUIRE(r2.getWidth() == Approx(9.2));
+}
+
+TEST_CASE ( "Rectangle/constains") {
+    Point p(0,0);
+    Point pp(20, 20);
+    REQUIRE(r2.constains(p) == true);
+    REQUIRE(r2.constains(pp) == false);
 }
 
